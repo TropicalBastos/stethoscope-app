75955af279dcd8e995232542047946b3161e5a06
Ian Bastos <ian-bastos@live.com>
2019-02-01 00:32:19 +0000

    custom policy class

---
+++ src/lib/CustomPolicy.js
@@ -0,0 +1,26 @@
+const STETHOSCOPE_POLICY = 'STETHOSCOPE_POLICY';
+
+class CustomPolicy{
+
+    constructor(){
+        let policy = localStorage.getItem(STETHOSCOPE_POLICY);
+        if(policy) {
+            this.policy = JSON.parse(policy);
+        }
+    }
+
+    getPolicy() {
+        return this.policy;
+    }
+
+    setPolicy(policy) {
+        localStorage.setItem(STETHOSCOPE_POLICY, JSON.stringify(policy));
+    }
+
+    hasCustomPolicy() {
+        return this.policy ? true : false;
+    }
+
+}
+
+export default CustomPolicy;
